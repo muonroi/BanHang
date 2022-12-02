37a40062430f9a4e617b6f00bc25959d1261013d
muonroi <leanhphi1706@gmail.com>
2022-12-02 17:20:12 +0700

    ...

--- eShopSolution.WebApp/wwwroot/js/site.js
+++ eShopSolution.WebApp/wwwroot/js/site.js
@@ -2,6 +2,7 @@
     this.initialize = function () {
         regsiterEvents();
         loadCart();
+        regsiterEvents2();
     }
     function loadCart() {
         const culture = $('#hidCulture').val();
@@ -17,6 +18,30 @@
     function regsiterEvents() {
         // Write your JavaScript code.
         $('body').on('click', '.btn-add-cart', function (e) {
+            e.preventDefault();
+            const culture = $('#hidCulture').val();
+            const id = $(this).data('id');
+            $.ajax({
+                type: "POST",
+                url: "/" + culture + '/Cart/AddToCart',
+                data: {
+                    id: id,
+                    languageId: culture
+                },
+                success: function (res) {
+                    $('#lblCartCount').text(res.length);
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+            alert("Thêm sản phẩm vào giỏ thành công");
+            window.location.reload();
+        });
+    }
+    function regsiterEvents2() {
+        // Write your JavaScript code.
+        $('body').on('click', '.add-to-cart-dt2', function (e) {
             e.preventDefault();
             const culture = $('#hidCulture').val();
             const id = $(this).data('id');
